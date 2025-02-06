ca2173230f43f580520286c8262deb6200b48a09
Benjamin Bengfort <benjamin@bengfort.com>
2025-02-06 08:34:44 -0600

    initial cli program

--- cmd/ulid/main.go
+++ cmd/ulid/main.go
@@ -1,3 +1,99 @@
 package main
 
-func main() {}
+import (
+	cryptorand "crypto/rand"
+	"flag"
+	"fmt"
+	mathrand "math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"go.rtnl.ai/ulid"
+)
+
+const (
+	defaultms = "Mon Jan 02 15:04:05.999 MST 2006"
+	rfc3339ms = "2006-01-02T15:04:05.000Z07:00"
+)
+
+func main() {
+	var (
+		format = flag.String("format", "default", "when parsing, show times in this format: default, rfc3339, unix, ms")
+		local  = flag.Bool("local", false, "when parsing, show local time instead of UTC")
+		quick  = flag.Bool("quick", false, "when generating, use non-crypto-grade entropy")
+		zero   = flag.Bool("zero", false, "when generating, fix entropy to all-zeroes")
+		help   = flag.Bool("help", false, "print this help text")
+	)
+
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	var formatFunc func(time.Time) string
+	switch strings.ToLower(*format) {
+	case "default":
+		formatFunc = func(t time.Time) string { return t.Format(defaultms) }
+	case "rfc3339":
+		formatFunc = func(t time.Time) string { return t.Format(rfc3339ms) }
+	case "unix":
+		formatFunc = func(t time.Time) string { return fmt.Sprint(t.Unix()) }
+	case "ms":
+		formatFunc = func(t time.Time) string { return fmt.Sprint(t.UnixNano() / 1e6) }
+	default:
+		fmt.Fprintf(os.Stderr, "invalid --format %s\n", *format)
+		os.Exit(1)
+	}
+
+	switch flag.NArg() {
+	case 0:
+		generate(*quick, *zero)
+	default:
+		parse(flag.Arg(0), *local, formatFunc)
+	}
+}
+
+func generate(quick, zero bool) {
+	entropy := cryptorand.Reader
+	if quick {
+		seed := time.Now().UnixNano()
+		source := mathrand.NewSource(seed)
+		entropy = mathrand.New(source)
+	}
+	if zero {
+		entropy = zeroReader{}
+	}
+
+	id, err := ulid.New(ulid.Timestamp(time.Now()), entropy)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s\n", id)
+}
+
+func parse(s string, local bool, f func(time.Time) string) {
+	id, err := ulid.Parse(s)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	t := ulid.Time(id.Time())
+	if !local {
+		t = t.UTC()
+	}
+	fmt.Fprintf(os.Stderr, "%s\n", f(t))
+}
+
+type zeroReader struct{}
+
+func (zeroReader) Read(p []byte) (int, error) {
+	for i := range p {
+		p[i] = 0
+	}
+	return len(p), nil
+}
